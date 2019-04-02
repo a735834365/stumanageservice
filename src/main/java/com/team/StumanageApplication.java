e5dfbab0c18f1853ab5b9ec8934345cfe3013b7e
ku_o <735834365@qq.com>
2019-04-02 16:56:20 +0800

    测试

--- src/main/java/com/team/StumanageApplication.java
+++ src/main/java/com/team/StumanageApplication.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StumanageApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(StumanageApplication.class, args);
     }
