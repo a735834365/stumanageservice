f0e5c23d5b70116cc387deb86282fa1ba4de408c
a735834365 <735834365@qq.com>
2019-04-07 13:47:14 +0800

    新增配置

--- src/main/java/com/team/config/WebSecurityConfig.java
+++ src/main/java/com/team/config/WebSecurityConfig.java
@@ -4,14 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team.common.beans.ResultBean;
 import com.team.common.utils.UserUtil;
 import com.team.exception.AuthenticationAccessDeniedHandler;
-import com.team.exception.CustomExceptionResolver;
 import com.team.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.*;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,7 +17,6 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
@@ -81,7 +78,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 })
                 .and()
                 .formLogin().loginPage("/login_p").loginProcessingUrl("/login")
-                .usernameParameter("username").passwordParameter("password")
+                .usernameParameter("username")
+                // 自定义登录请求，即表单中的action
+                .passwordParameter("password")
                 .failureHandler(new AuthenticationFailureHandler() {
                     @Override
                     public void onAuthenticationFailure(HttpServletRequest req,
