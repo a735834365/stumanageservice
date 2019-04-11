391d2f9c148401b99222655ab8de460b9dc73c0b
“ne <735834365@qq.com>
2019-04-11 19:30:06 +0800

    重构代码，前后端分离

---
+++ src/main/java/com/team/web/controller/student/StudentBasicController.java
@@ -0,0 +1,18 @@
+package com.team.web.controller.student;
+
+import com.team.common.beans.ResultBean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * create by yifeng
+ */
+@RestController
+@RequestMapping("/student/basic")
+public class StudentBasicController {
+
+//    @RequestMapping(value = "basicdata", method = RequestMethod.GET)
+//    public ResultBean<>
+
+}
